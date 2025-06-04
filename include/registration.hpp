b3039153d2af3acbef8086b2177d27ef2b20ba26
prfAguiar <prf.aguiar@gmail.com>
2025-06-04 08:02:24 -0300

    functions for player registration

--- include/registration.hpp
+++ include/registration.hpp
@@ -2,27 +2,49 @@
 #define REGISTRATION_HPP
 #include <fstream>
 #include <string>
+#include <vector>
+
+struct player{
+  std::string username;
+  int score, games;
+};
 
 class registration {
 private:
   std::fstream users;
-  std::fstream champion;
+  std::string file;
+  bool openfile_check = 0;
+  std::streampos pos_champion;
+  int score_champion = 0;
+  std::vector<player> players;
 
+
+//score, username, password, games 
 public:
-  registration(std::string arquivo, std::string champion);
+  registration(std::string arquivo);
+  bool isFileEmpty();
 
   std::fstream get_users();
-  std::fstream get_champion();
+  // std::fstream get_champion();
+  int getline_number(std::string user);
 
-  void new_user(std::string name);
+  void new_user(std::string name, std::string password, int score, int games);
   // void list_users();
   std::string get_stats(std::string name); // retorna "null" se não existe
-
-  int update(std::string user, int score);
+  std::vector<player> get_all(); //resgata as informações de todos os jogadores, exceto senha
+  void update(std::string user, int score);
+  void delete_user(std::string user);
 
   int get_max_score();
+  // void getpos_champion();
   std::string get_max_user();
-  void update_champion(std::string new_champion, int new_max);
+  void update_champion();
+
+  bool isOpenFile(){
+    return openfile_check;
+  }
+
+  ~registration();
 };
 
 #endif // REGISTRATION_HPP
