f459bcb12f2f17c00c3b362166e0344546bfaebd
samhigino <samuelhralmeida@gmail.com>
2025-06-24 23:35:54 -0300

    Doxygen comments

--- include/game_object.hpp
+++ include/game_object.hpp
@@ -4,6 +4,10 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 
+/**
+ * @struct absolute_position
+ * @brief Estrutura que armazena a posição e dimensão de um objeto no jogo
+ */
 typedef struct absolute_position {
     float x;  //posição horizontal do ponto central do objeto
     float y;  //posição vertical do ponto central do objeto
@@ -11,21 +15,61 @@ typedef struct absolute_position {
     int h;    //altura (height)
 } abs_pos;
 
+/**
+ * @class button
+ * @brief Classe genérica que representa os objetos do jogo
+ */ 
 class game_object {
     private:
-        absolute_position position;
-        ALLEGRO_BITMAP* Bitmap;
+        absolute_position position; ///< Posição absoluta do objeto no jogo
+        ALLEGRO_BITMAP* Bitmap;     ///< Bitmap(sprite) do objeto
 
     public:
-        //Construtor
-        game_object(float a, float b, int c, int d, ALLEGRO_BITMAP* e);
+        /**
+         * @brief Construtor da classe game_object
+         * @param x Posição X do objeto
+         * @param y Posição Y do objeto
+         * @param w Largura do objeto
+         * @param h Altura do objeto
+         * @param bitmap Bitmap(sprite) do objeto
+         */
+        game_object(float x, float y, int w, int h, ALLEGRO_BITMAP* bitmap);
 
-        //Métodos padrão
+        /**
+         * @brief Retorna a posição absoluta do objeto
+         * @return Ponteiro para a estrutura absolute_position que contém a posição e dimensão do objeto
+         */
         abs_pos* Get_position();
+
+        /**
+         * @brief Retorna o bitmap(sprite) do objeto
+         * @return Bitmap do objeto
+         */
         ALLEGRO_BITMAP* get_bitmap();
+
+        /**
+         * @brief Detecta colisão entre este objeto e outro objeto
+         * @param other_object Referência ao outro objeto a ser verificado a colisão
+         * @return True se está colidindo, false caso contrário
+         */
         bool is_colliding(game_object* other_object);
+
+        /**
+         * @brief Troca o bitmap(sprite) do objeto
+         * @param sprite Nome do arquivo de imagem a ser carregado como bitmap
+         */
         void set_bitmap(std::string sprite);
+
+        /**
+         * @brief Troca o bitmap(sprite) do objeto
+         * @param sprite Ponteiro para o bitmap a ser usado
+         */
         void set_bitmap(ALLEGRO_BITMAP* sprite);
+
+        /**
+         * @brief Desenha o objeto na tela
+         * @param scale Escala de desenho do objeto
+         */
         void Draw(float scale);
 };
 
--- include/image.hpp
+++ include/image.hpp
@@ -2,38 +2,116 @@
 #define IMAGE_H
 #include "ui_object.hpp"
 
+/**
+ * @class image
+ * @brief Classe que representa uma imagem no jogo
+ * 
+ * Esta classe é usada exatamente como uma imagem, não possui interações mas pode se mover.
+ */
 class image {
     private:
-        ALLEGRO_BITMAP* bitmap;
-        float vel_x;
-        float vel_y;
-        float acel_x;
-        float acel_y;
-        float x, y;
-        int width, height;
+        ALLEGRO_BITMAP* bitmap; ///< Bitmap(sprite) da imagem
+        float vel_x;            ///< Velocidade horizontal da imagem
+        float vel_y;            ///< Velocidade vertical da imagem
+        float acel_x;           ///< Aceleração horizontal da imagem
+        float acel_y;           ///< Aceleração vertical da imagem
+        float x, y;             ///< Posição X e Y da imagem
+        int width, height;      ///< Largura e altura da imagem
     
     public:
-        //Construtores
+        /**
+         * @brief Construtor da classe image
+         * @param sprite Bitmap(sprite) da imagem
+         * @param x Posição X da imagem
+         * @param y Posição Y da imagem
+         * @details Este construtor inicializa velocidade e aceleração como 0, e largura e altura como as dimensões do bitmap.
+         */
         image(ALLEGRO_BITMAP* sprite, float x, float y) : image(sprite, x, y, 0, 0, 0, 0) { };
+
+        /**
+         * @brief Construtor da classe image
+         * @param sprite Bitmap(sprite) da imagem
+         * @param x Posição X da imagem
+         * @param y Posição Y da imagem
+         * @param vx Velocidade horizontal da imagem
+         * @param vy Velocidade vertical da imagem
+         * @param ax Aceleração horizontal da imagem
+         * @param ay Aceleração vertical da imagem
+         * @details Este construtor inicializa a largura e altura como as dimensões do bitmap.
+         */
         image(ALLEGRO_BITMAP* sprite, float x, float y, float vx, float vy, float ax, float ay);
 
-        //Métodos
+        /**
+         * @brief Desenha a imagem na tela
+         */
         void Draw();
+
+        /**
+         * @brief Desenha a imagem na tela com escala
+         * @param scale Fator de escala para desenhar a imagem
+         */
         void Draw(float scale);
+
+        /**
+         * @brief Atualiza a posição da imagem com base na velocidade e aceleração
+         */
         void Update();
 
-        //Getters
-        float get_x();                              // retorna a posição X
-        float get_y();                              // retorna a posição Y
-        float get_velocity_x();                     // retorna a velocidade X
-        float get_velocity_y();                     // retorna a velocidade Y
+        /**
+         * @brief Retorna a posição X da imagem
+         * @return Coordenada X de posição imagem
+         */
+        float get_x();
 
-        //Setters
-        void set_position_x(float new_x);                               // seta a posição X
-        void set_position_y(float new_y);                               // seta a posição Y
-        void set_velocity(float new_vel_x, float new_vel_y);            // seta a velocidade
-        void set_acceleration(float new_accel_x, float new_accel_y);    // seta a aceleração
-        void set_bitmap(ALLEGRO_BITMAP* new_sprite);                    // seta um novo sprite
+        /**
+         * @brief Retorna a posição Y da imagem
+         * @return Coordenada Y de posição imagem
+         */
+        float get_y();
+
+        /**
+         * @brief Retorna a velocidade horizontal da imagem
+         * @return Velocidade X da imagem
+         */
+        float get_velocity_x();
+
+        /**
+         * @brief Retorna a velocidade vertical da imagem
+         * @return Velocidade Y da imagem
+         */
+        float get_velocity_y();
+
+        /**
+         * @brief Seta a posição X da imagem
+         * @param new_x Nova posição X a ser definida
+         */
+        void set_position_x(float new_x);
+
+        /**
+         * @brief Seta a posição Y da imagem
+         * @param new_y Nova posição Y a ser definida
+         */
+        void set_position_y(float new_y);
+
+        /**
+         * @brief Seta a velocidade da imagem
+         * @param new_vel_x Nova velocidade horizontal a ser definida
+         * @param new_vel_y Nova velocidade vertical a ser definida
+         */
+        void set_velocity(float new_vel_x, float new_vel_y);
+
+        /**
+         * @brief Seta a aceleração da imagem
+         * @param new_accel_x Nova aceleração horizontal a ser definida
+         * @param new_accel_y Nova aceleração vertical a ser definida
+         */
+        void set_acceleration(float new_accel_x, float new_accel_y);
+        
+        /**
+         * @brief Troca o bitmap(sprite) da imagem
+         * @param new_sprite Novo bitmap a ser usado
+         */
+        void set_bitmap(ALLEGRO_BITMAP* new_sprite);
 };
 
 #endif
